291694dbbcdc83e331e0d2845712072e4df19ca8
maulikaluthra <56387512+maulikaluthra@users.noreply.github.com>
2020-10-20 18:48:32 +0530

    Update Functions.cpp

--- c++/introduction/Functions.cpp
+++ c++/introduction/Functions.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int max_of_four(int, int, int, int);
-int max_of_four(int a, int b, int c, int d)
-{
-    return max(max(a,b),max(c,d));             }
+int max_of_four(int a, int b, int c, int d) {
+    return max(max(a,b),max(c,d));        //max is an inbuilt function
+}
 
 
 int main() {
     int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
+    cin >> a >> b >> c >> d;
     int ans = max_of_four(a, b, c, d);
     printf("%d", ans);
     
